4c6aec62e1009467fc89e8dd3136ca6c8b079dc0
TunaOzk <tunaozkoc325@gmail.com>
2024-01-21 15:02:32 +0300

    feat: background and header are introduced

---
+++ src/app/home/page.tsx
@@ -0,0 +1,8 @@
+import Header from "@/components/Header";
+import styles from "./page.module.css";
+
+export default function HomePage() {
+    return <main className={styles.background}>
+        <Header />
+    </main>
+}
