96010042bebdd404d216f27648a78479bdeadc25
TunaOzk <tunaozkoc325@gmail.com>
2024-02-04 12:28:47 +0300

    feat: added CategoryList component

---
+++ src/components/CategoryList.tsx
@@ -0,0 +1,75 @@
+'use client';
+import React, {FC} from "react";
+import styles from "../styles/categorylist.module.css";
+import Image, { StaticImageData } from 'next/image'
+import hotel from "../assets/hotel.png"
+import restaurant from "../assets/restaurant.png"
+
+interface ListItem {
+    outerClassName: string,
+    name: string,
+    innerClassName: string,
+    src: StaticImageData,
+    title: string
+}
+
+const listItems: ListItem[] = [
+    {
+        outerClassName: "hotels_div",
+        name: "hotel",
+        innerClassName: "hotel_png_div",
+        src: hotel,
+        title: "Hotels"
+    },
+    {
+        outerClassName: "rests_div",
+        name: "rest",
+        innerClassName: "rest_png_div",
+        src: restaurant,
+        title: "Restaurants"
+    }
+]
+
+const CategoryList: FC<{}> = () => {
+    return (
+    <div className={styles.categories}>
+        {listItems.map((item) => (
+            <div className={styles[item.outerClassName]} key={item.name}>
+                <div className={styles[item.name]}>
+                    <div className={styles[item.innerClassName]}>
+                        <div className={styles.category_png}>
+                            <Image src={item.src} alt={item.name} width={60} height={60}></Image>
+                        </div>
+                    </div>
+                    <p className={styles.category_title}>{item.title}</p>
+                </div>
+            </div>
+        ))}
+        {/* <div className={styles.hotels_div}>
+            <div className={styles.hotel}>
+                <div className={styles.hotel_png_div}>
+                    <div className={styles.category_png}>
+                        <Image src={hotel} alt="hotel" width={60} height={60}></Image>
+                    </div>
+                </div>
+                <p className={styles.category_title}>Hotels</p>
+            </div>
+        </div>
+        <div className={styles.rests_div}>
+            <div className={styles.rest}>
+                <div className={styles.rest_png_div}>
+                    <div className={styles.category_png}>
+                        <Image src={restaurant} alt="restaurant" width={60} height={60}></Image>
+                    </div>
+                </div>
+                <p className={styles.category_title}>Restaurants</p>
+            </div>
+        </div>
+        <div>
+
+        </div> */}
+    </div>
+    )
+}
+
+export default CategoryList;
