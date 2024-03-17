9d3bc789f871b79558efddf20f4a89de0978f6d6
TunaOzk <tunaozkoc325@gmail.com>
2024-03-17 13:07:13 +0300

    feat: routing to searchpage is completed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,30 +1,46 @@
-'use client';
-import React, {FC} from "react";
-import styles from  "../styles/header.module.css";
+"use client";
+import React, { FC } from "react";
+import styles from "../styles/header.module.css";
+import { Button } from "@mui/material";
+import { Search } from "@mui/icons-material";
+import { useRouter } from "next/navigation";
 
-const navItemTitles: string[] = ['Home', 'About', 'Discover', 'Blog', 'Contact']
+const navItemTitles: string[] = [
+  "Home",
+  "About",
+  "Discover",
+  "Blog",
+  "Contact",
+];
 
 const Header: FC<{}> = () => {
-    return (
+  const router = useRouter();
+  return (
     <div className={styles.header}>
-        <div className={styles.header_box}>
-            <p className={styles.title}>
-                On trip
-            </p>
-            <div className={styles.nav_box}>
-                <div className={styles.nav}>
-                    {navItemTitles.map((item_name) => {
-                        return (
-                        <button
-                            className={styles.nav_item} key={item_name}>{item_name}
-                        </button>
-                        )
-                    } )}
-                </div>
-            </div>
+      <div className={styles.header_box}>
+        <p className={styles.title}>On trip</p>
+        <div className={styles.nav_box}>
+          <div className={styles.nav}>
+            {navItemTitles.map((item_name) => {
+              return (
+                <button className={styles.nav_item} key={item_name}>
+                  {item_name}
+                </button>
+              );
+            })}
+          </div>
         </div>
+        <Button
+          onClick={() => router.push("/search")}
+          className={styles.explore_button}
+          variant="contained"
+          endIcon={<Search fontSize="large" />}
+        >
+          Explore
+        </Button>
+      </div>
     </div>
-    )
-}
+  );
+};
 
 export default Header;
